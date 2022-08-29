687aae528855f8341fcc4bab32ea419cb2c22218
Erno Hentonen <erno.hentonen@opiframe.com>
2022-08-29 13:19:29 +0300

    helloworld 2

--- javascript/01_helloworld/server.js
+++ javascript/01_helloworld/server.js
@@ -4,6 +4,10 @@ let app = express();
 
 app.use(express.static("public"));
 
+app.get("/greeting/:name",function(req,res) {
+	return res.status(200).json({"greeting":"Hello "+req.params.name})
+})
+
 app.listen(3000);
 
 console.log("Running in port 3000");
