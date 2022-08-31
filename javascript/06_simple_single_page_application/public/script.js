205f9f1cafb20f3f12ccc4aeec3060bdb464db05
Erno Hentonen <erno.hentonen@opiframe.com>
2022-08-31 15:01:47 +0300

    table works

--- javascript/06_simple_single_page_application/public/script.js
+++ javascript/06_simple_single_page_application/public/script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 	createForm();
+	getContactList();
 }
 
 var mode = 0;
@@ -104,7 +105,7 @@ addContact = async () => {
 		lastname.value="";
 		email.value="";
 		phone.value="";
-		//TODO get contact list
+		getContactList();
 	} else {
 		console.log("Server responded with a status:",response.status);
 	}
@@ -123,6 +124,95 @@ getContactList = async () => {
 	} else {
 		console.log("Server responded with a status:"+response.status);
 	}
+}
+
+populateTable = (data) => {
+	let anchor = document.getElementById("anchor");
+	let oldTable = document.getElementById("table");
+	if(oldTable) {
+		anchor.removeChild(oldTable);
+	}
+	let table = document.createElement("table");
+	
+	//Table header
+	let header = document.createElement("thead");
+	let headerRow = document.createElement("tr");
+	
+	let firstnameheader = document.createElement("th");
+	let firstnametext = document.createTextNode("First Name");
+	firstnameheader.appendChild(firstnametext);
+	
+	let lastnameheader = document.createElement("th");
+	let lastnametext = document.createTextNode("Last Name");
+	lastnameheader.appendChild(lastnametext);
+	
+	let emailheader = document.createElement("th");
+	let emailtext = document.createTextNode("Email");
+	emailheader.appendChild(emailtext);
+	
+	let phoneheader = document.createElement("th");
+	let phonetext = document.createTextNode("Phone");
+	phoneheader.appendChild(phonetext);
+	
+	let removeheader = document.createElement("th");
+	let removetext = document.createTextNode("Remove");
+	removeheader.appendChild(removetext);
+	
+	let editheader = document.createElement("th");
+	let edittext = document.createTextNode("Edit");
+	editheader.appendChild(edittext);
+	
+	headerRow.appendChild(firstnameheader);
+	headerRow.appendChild(lastnameheader);
+	headerRow.appendChild(emailheader);
+	headerRow.appendChild(phoneheader);
+	headerRow.appendChild(removeheader);	
+	headerRow.appendChild(editheader);
+
+	header.appendChild(headerRow);
+	table.appendChild(header);
+	
+	//table body
+	let tableBody = document.createElement("tbody");
+
+	for(let i=0;i<data.length;i++) {
+		let tableRow = document.createElement("tr");
+		for(x in data[i]) {
+			if(x === "id") {
+				continue;
+			}
+			let column = document.createElement("td");
+			let info = document.createTextNode(data[i][x]);
+			column.appendChild(info);
+			tableRow.appendChild(column);
+		}
+		//remove column
+		let removeColumn = document.createElement("td");
+		let removeButton = document.createElement("input");
+		removeButton.setAttribute("type","button");
+		removeButton.setAttribute("value","Remove");
+		removeButton.setAttribute("name",data[i].id);
+		removeButton.addEventListener("click",function(event) {
+			//TODO removeContact(event.target.name);
+		})
+		removeColumn.appendChild(removeButton);
+		
+		//edit column
+		let editColumn = document.createElement("td");
+		let editButton = document.createElement("input");
+		editButton.setAttribute("type","button");
+		editButton.setAttribute("value","Edit");
+		editButton.addEventListener("click",function(event) {
+			//TODO changeToEditMode(data[i]);
+		})
+		editColumn.appendChild(editButton);
+		tableRow.appendChild(removeColumn);
+		tableRow.appendChild(editColumn);
+		tableBody.appendChild(tableRow);
+	}
+	table.appendChild(tableBody);
+	table.setAttribute("id","table");
+	anchor.appendChild(table);
 }
 
 
