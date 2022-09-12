be4d2b1d0f68069cab64b470c24ae35374930f4b
Erno Hentonen <erno.hentonen@opiframe.com>
2022-09-12 14:35:54 +0300

    shopping app 1 frontend

--- react/50_shopping_app/01_stage/frontend/src/App.js
+++ react/50_shopping_app/01_stage/frontend/src/App.js
@@ -2,6 +2,8 @@ import {useState,useEffect} from 'react';
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import Navbar from './components/Navbar';
 
 function App() {
 	
@@ -80,8 +82,11 @@ function App() {
 	
 	return (
 		<div className="App">
+			<Navbar/>
+			<hr/>
 			<Routes>
-				<Route exact path="/" element={<ShoppingForm addItem={addItem}/>}/>
+				<Route exact path="/" element={<ShoppingList list={state.list}/>}/>
+				<Route path="/form" element={<ShoppingForm addItem={addItem}/>}/>
 			</Routes>
 		</div>
 	);
--- react/50_shopping_app/01_stage/frontend/src/components/Navbar.js
+++ react/50_shopping_app/01_stage/frontend/src/components/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = (props) => {
 	
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand">Shopping App</a>
+			<a className="navbar-brand" style={{marginLeft:10}}>Shopping App</a>
 			<ul className="navbar-nav">
 				<li className="nav-item">
 					<Link to="/">Shopping List</Link>
 				</li>
-				<li className="nav-item">
+				<li className="nav-item" style={{marginLeft:10}}>
 					<Link to="/form">Add new item</Link>
 				</li>
 			</ul>		
