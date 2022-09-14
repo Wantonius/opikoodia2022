1075950cc70d5c60f0eeb097660836037861823b
Erno Hentonen <erno.hentonen@opiframe.com>
2022-09-14 14:21:56 +0300

    shopping app 2 frontend

---
+++ react/10_usememo_hook/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState,useMemo} from 'react';
+
+function App() {
+	
+	const [count,setCount] = useState(0);
+	const [currentWord,setCurrentWord] = useState(0);
+	const words = ["banaani","omena","olut","jäätelö"];
+	const word = words[currentWord];
+	
+	const computeWordLength = (word) => {
+		let i=0;
+		while(i<1000000000) {
+			i++;
+		}
+		return word.length;
+	}
+	
+	const wordLength = computeWordLength(word);
+	
+	return (
+		<div className="App">
+			<h3>Compute the length of the word {word}</h3>
+			<h3>{word} has {wordLength} letters</h3>
+			<button onClick={() => {
+				const next = currentWord + 1 === words.length ? 0 : currentWord+1;
+				setCurrentWord(next);
+			}}>Next Word</button>
+		
+			<h3>Increment counter</h3>
+			<h3>Current value:{count}</h3>
+			<button onClick={() => setCount(count => count +1)}>Increment</button>
+		</div>
+	);
+}
+
+export default App;
