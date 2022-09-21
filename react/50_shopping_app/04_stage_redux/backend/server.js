da9dcb651e8f2a6f8a60eecdc34c5c98fb8640c8
Erno Hentonen <erno.hentonen@opiframe.com>
2022-09-21 14:22:33 +0300

    mongodb url

--- react/50_shopping_app/04_stage_redux/backend/server.js
+++ react/50_shopping_app/04_stage_redux/backend/server.js
@@ -18,7 +18,11 @@ const mongo_user = process.env.MONGODB_USER;
 const mongo_password = process.env.MONGODB_PASSWORD;
 const mongo_url = process.env.MONGODB_URL;
 
-mongoose.connect("mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/shoppingdatabase?retryWrites=true&w=majority").then(
+let url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/shoppingdatabase?retryWrites=true&w=majority"
+
+console.log("MONGODB URL",url);
+
+mongoose.connect(url).then(
 	() => console.log("Connected to mongodb"),
 	(err) => console.log("Failed to connect. Reason",err)
 );
