5be61dbdce71bf52dd7463182d4a1996024478c0
Erno Hentonen <erno.hentonen@opiframe.com>
2022-09-26 09:14:59 +0300

    shopping redux

---
+++ react/50_shopping_app/04_stage_redux/frontend/src/actions/shoppingActions.js
@@ -0,0 +1,12 @@
+import {loading,stopLoading,clearState} from './loginActions';
+
+//Action constants
+
+export const FETCH_LIST_SUCCESS = "FETCH_LIST_SUCCESS";
+export const FETCH_LIST_FAILED = "FETCH_LIST_FAILED";
+export const ADD_ITEM_SUCCESS = "ADD_ITEM_SUCCESS";
+export const ADD_ITEM_FAILED = "ADD_ITEM_FAILED";
+export const REMOVE_ITEM_SUCCESS = "REMOVE_ITEM_SUCCESS";
+export const REMOVE_ITEM_FAILED = "REMOVE_ITEM_FAILED";
+export const EDIT_ITEM_SUCCESS = "EDIT_ITEM_SUCCESS";
+export const EDIT_ITEM_FAILED = "EDIT_ITEM_FAILED";
