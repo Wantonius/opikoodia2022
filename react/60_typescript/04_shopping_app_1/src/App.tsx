befc5ad1b96f6691a7ed4954620a5862815ed5bf
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-03 13:41:07 +0300

    shopping app typescript

--- react/60_typescript/04_shopping_app_1/src/App.tsx
+++ react/60_typescript/04_shopping_app_1/src/App.tsx
@@ -1,47 +1,27 @@
-import React,{useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ShoppingItem from './models/ShoppingItem';
 import ShoppingForm from './components/ShoppingForm';
 import ShoppingList from './components/ShoppingList';
+import {useAction} from './hooks/useAction'
 
-interface State {
-	list:ShoppingItem[];
-	id:number;
-}
 
 function App() {
 	
-	const [state,setState] = useState<State>({
-		list:[],
-		id:100
-	})
+	const [list,loading,addToList,removeFromList] = useAction();
 	
-	const addToList = (item:ShoppingItem) => {
-		item.id = state.id;
-		setState((state) => {
-			return {
-				list:state.list.concat(item),
-				id:state.id+1
-			}
-		})
-	}
-	
-	const removeFromList = (id:number) => {
-		setState((state) => {
-			let tempList = state.list.filter(item => item.id !== id)
-			return {
-				...state,
-				list:tempList
-			}
-		})
+	let header = <h2>Shopping App</h2>
+	if(loading) {
+		header = <h2>Loading ...</h2>
 	}
 	
 	return (
 		<div className="App">
+			{header}
 			<ShoppingForm addToList={addToList}/>
 			<hr/>
-			<ShoppingList list={state.list} removeFromList={removeFromList}/>
+			<ShoppingList list={list} removeFromList={removeFromList}/>
 		</div>
 	);
 }
--- react/60_typescript/04_shopping_app_1/src/hooks/useAction.tsx
+++ react/60_typescript/04_shopping_app_1/src/hooks/useAction.tsx
@@ -45,3 +45,75 @@ const listReducer = (state:AppState,action:Action) => {
 			return state;
 	}
 }
+
+export const useAction = ():[ShoppingItem[],boolean,(item:ShoppingItem) => void,(id:number) => void] => {
+	
+	const [urlRequest,setUrlRequest] = useState<FetchState>({
+		request:new Request("",{})
+	}) 
+	
+	const [state,dispatch] = useReducer(listReducer,initialState);
+	
+	useEffect(() => {
+		if(!urlRequest.request) {
+			return;
+		}
+		
+		const fetchData = async () => {
+			dispatch({type:"LOADING"});
+			const response = await fetch(urlRequest.request);
+			dispatch({type:"STOP_LOADING"});
+			if(!response) {
+				return;
+			}
+			if(response.ok) {
+				if(urlRequest.request.method === "GET") {
+					const data = await response.json();
+					dispatch({
+						type:"FETCH_DONE",
+						payload:data
+					})
+				} else {
+					getList();
+				}
+			} else {
+				console.log("Server responded with a status ",response.status,response.statusText)
+			}
+			
+		}
+		
+		fetchData();
+	},[urlRequest.request])
+
+	const getList = () => {
+		let tempRequest = new Request("/api/shopping",{
+			method:"GET"
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+
+	const addToList = (item:ShoppingItem) => {
+		let tempRequest:Request = new Request("/api/shopping",
+		{
+			method:"POST",
+			headers:{"Content-Type":"application/json"},
+			body:JSON.stringify(item)
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+	
+	const removeFromList = (id:number) => {
+		let tempRequest:Request = new Request("/api/shopping/"+id,{
+			method:"DELETE"
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+	
+	return [state.list,state.loading,addToList,removeFromList];
+}
