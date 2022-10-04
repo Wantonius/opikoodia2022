41aeda7e64cf9f3d77b4070099116197ee80e132
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-04 14:30:07 +0300

    typescript

--- react/60_typescript/06_shopping_with_redux/src/components/ShoppingForm.tsx
+++ react/60_typescript/06_shopping_with_redux/src/components/ShoppingForm.tsx
@@ -19,6 +19,65 @@ interface State {
 
 const ShoppingForm:React.FC<{}> = (props) => {
 	
+	const [state,setState] = useState<State>({
+		type:"",
+		count:0,
+		price:0
+	})
+	
+	const tokenSelector = (state:TokenState) => state.login.token
+	
+	const token = useSelector(tokenSelector);
+
+	const dispatch:ThunkDispatch<any,any,AnyAction> = useDispatch();
+	
+	const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+		setState((state) => {
+			return {
+				...state,
+				[e.target.name]:e.target.value
+			}
+		})
+	}
+	
+	const onSubmit = (e:React.SyntheticEvent) => {
+		e.preventDefault();
+		let item = new ShoppingItem(0,state.type,state.count,state.price);
+		dispatch(add(token,item));
+		setState({
+			type:"",
+			count:0,
+			price:0
+		})
+	}
+	
+	return(
+		<form onSubmit={onSubmit}>
+			<label htmlFor="type">Type</label>
+			<input type="text"
+					name="type"
+					id="type"
+					onChange={onChange}
+					value={state.type}/>
+			<br/>
+			<label htmlFor="count">Count</label>
+			<input type="number"
+					name="count"
+					id="count"
+					onChange={onChange}
+					value={state.count}/>
+			<br/>
+			<label htmlFor="price">Price</label>
+			<input type="number"
+					name="price"
+					id="price"
+					step="0.01"
+					onChange={onChange}
+					value={state.price}/>
+			<br/>
+			<input type="submit" value="Add"/>
+		</form>
+	)
 }
 
 export default ShoppingForm;
